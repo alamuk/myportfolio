14b594b673aa3e9b30d80df07068038881fa3a0f
alamuk <alam.2008@live.co.uk>
2025-09-02 13:11:35 +0100

    updated

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,3 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
-}
-
-export default config
+/** @type {import('next').NextConfig} */
+const nextConfig = { output: 'export' };
+export default nextConfig;
